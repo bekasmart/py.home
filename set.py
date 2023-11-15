76182f9b390de3c35738e34ce9f60f4640011582
bekasmart <149644433+bekasmart@users.noreply.github.com>
2023-11-15 22:22:33 +0600

    home

---
+++ set.py
@@ -0,0 +1,21 @@
+citiz= {"alvfti","astana", "aktay", "alatau" ,"povladar"}
+# print(len(citiz))
+# citiz.add("tokyo")
+# print(len(citiz))
+
+# new_city =input("America")
+# citiz.add("new_siti")
+# print(citiz)
+
+# new_city=input("Enter a city:")
+
+# if new_city in citiz:
+#     citiz.remove(new_city)
+#     print(f"{new_city} was removed from the set")
+# else: 
+#     print("No such city")
+
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
+it_companies.add("Twitter")
+print(it_companies)
